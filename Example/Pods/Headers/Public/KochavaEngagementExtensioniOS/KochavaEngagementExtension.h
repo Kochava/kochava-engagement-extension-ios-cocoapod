6543bff87372b11caf08903be6c85e4c7ff02b1a
John Bushnell <jbushnell@kochava.com>
2019-05-24 15:17:21 -0700

    Miscellaneous updates.

---
+++ Example/Pods/Headers/Public/KochavaEngagementExtensioniOS/KochavaEngagementExtension.h
@@ -0,0 +1,1 @@
+../../../../../KochavaEngagementExtensioniOS/Classes/KochavaEngagementExtension.h
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,5 +1,4 @@
 import XCTest
-import KochavaEngagementExtensioniOS
 
 class Tests: XCTestCase {
     
